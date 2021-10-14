b9aa84b8b77f030864572bdce255f919a7c68651
Kirt72114 <kirt72114@aol.com>
2021-10-14 07:10:52 -0400

    Update DynamicTable3.vue

--- src/components/Custom/DynamicTable3.vue
+++ src/components/Custom/DynamicTable3.vue
@@ -7,63 +7,14 @@
             <b-col cols="4" class="float-left">
               <b-pagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage" class="my-0"></b-pagination>
             </b-col>
-            <b-col cols="4">
+            <b-col v-if="currentUser.isTerminologist" cols="4">
+              <!-- <input onclick="javascript:ShowPopupDialog('/terminology/Lists/Terminology/NewTerm.aspx');" type="button" value="New Term/Acronym" /> -->
               <b-button v-if="currentUser.isTerminologist" @click="$bvModal.show('bv-modal-addTerm')" style="height: 35px;">New Term/Acronym</b-button>
-              <b-modal id="bv-modal-addTerm" :no-close-on-backdrop="true" hide-footer>
-                <template #addNewTerm>
-                  Add New Term/Acronym
-                </template>
-                <b-container v-if="formReady" fluid class="contentHeight m-0 p-0">
-                  <b-row no-gutters class="contentHeight">
+              <b-modal size="xl" id="bv-modal-addTerm" :no-close-on-backdrop="true" hide-footer scrollable title="Add New Term/Acronym!">
+                <b-container size="xl" fluid class="m-0 p-0" :style="getStyle('modalheight', null)">
+                  <b-row no-gutters class="text-center">
                     <b-col cols="12" class="m-0 p-1">
-                      <b-card no-body>
-                        <b-form class="mt-0">
-                          <b-form-row>
-                            <b-col cols="2" class="text-center text-dark p-1">
-                              <b-form-group label="Branch" label-for="ddBranch" invalid-feedback="Please select a Branch" :state="ValidateMe('Branch')">
-                                <b-form-select
-                                  class="form-control"
-                                  size="sm"
-                                  id="ddBranch"
-                                  v-model="branch"
-                                  :options="branches"
-                                  @change="onBranchSelect"
-                                  :state="ValidateMe('Branch')"
-                                  ref="branch"
-                                  v-b-tooltip.hover.v-dark
-                                  title="Indicates the branch of the library to which the publication belongs. Supports filtering of publications."
-                                ></b-form-select>
-                              </b-form-group>
-                            </b-col>
-                            <b-col cols="2" class="text-center text-dark p-1">
-                              <b-form-group label="Prefix" label-for="ddPrefix" invalid-feedback="Please select a Prefix" :state="ValidateMe('Prefix')">
-                                <b-form-select class="form-control" size="sm" id="ddPrefix" v-model="prfx" :options="prefixes" :state="ValidateMe('Prefix')" ref="prfx"></b-form-select>
-                              </b-form-group>
-                            </b-col>
-                            <b-col cols="2" class="text-center text-dark p-1">
-                              <b-form-group label="Pub ID" label-for="txtPubID" invalid-feedback="Please enter a PubID" :state="ValidateMe('PubID')">
-                                <b-form-input class="form-control" size="sm" id="txtPubID" v-model="pubid" placeholder="Enter Pub ID" ref="pubid" :state="ValidateMe('PubID')"></b-form-input>
-                              </b-form-group>
-                            </b-col>
-                            <b-col cols="6" class="text-center text-dark p-1">
-                              <b-form-group label="Long Title" label-for="txtLongTitle" invalid-feedback="Please enter a Title" :state="ValidateMe('LongTitle')">
-                                <b-form-input class="form-control" size="sm" id="txtLongTitle" v-model="ltitle" placeholder="Enter Long Title" ref="ltitle" :state="ValidateMe('LongTitle')"></b-form-input>
-                              </b-form-group>
-                            </b-col>
-                          </b-form-row>
-                        </b-form>
-                        <template #footer>
-                          <b-row no-gutters>
-                            <b-col cols="10"></b-col>
-                            <b-col cols="2">
-                              <b-button-group>
-                                <b-button size="sm" @click="onCancel">Cancel</b-button>
-                                <b-button :disabled="!saveReady" size="sm" @click="onSave" variant="success">Save</b-button>
-                              </b-button-group>
-                            </b-col>
-                          </b-row>
-                        </template>
-                      </b-card>
+                      <iframe type="iframe" :style="getStyle('iframeheight', null)" width="100%" src="/terminology/Lists/Terminology/NewTerm.aspx?IsDlg=1"></iframe>
                     </b-col>
                   </b-row>
                 </b-container>
@@ -340,6 +291,14 @@ export default class DynamicTable extends Vue {
         style.height = '50px'
         style.width = that.contentwidth + 'px'
         break
+
+      case 'modalheight':
+        style.height = that.contentheight - 200 + 'px'
+        break
+
+      case 'iframeheight':
+        style.height = that.contentheight - 200 + 'px'
+        break
     }
     return style
   }
@@ -367,102 +326,6 @@ export default class DynamicTable extends Vue {
     }
     return h
   }
-  // Need more information from Terminologist to develop new form modal
-  /* public onCancel() {
-    // close the form
-    this.$router.push({ name: 'All Publications' })
-  }
-
-  public onSave() {
-    this.overlayText = 'Uploading Document'
-    this.formReady = false
-    // need to get the digest first
-    clearInterval(this.interval)
-    this.getDigest().then(response => {
-      if (response) {
-        this.interval = setInterval(this.saveForm, 500)
-      }
-    })
-  }
-
-  public async saveForm() {
-    // use the default new publication template to create a new document and upload it to the library.
-    // the return of the call will return the id of the new document which we will use to update the data
-    // the library is determined by the current user. If they are NATO, then the document is a Nato document
-    clearInterval(this.interval)
-    let nato: any = this.currentUser.isNATOLibrarian
-    this.nato = nato
-    let url = ''
-    let turl = ''
-    let getfileUrl = tp1 + slash + slash + tp2 + "/_api/web/GetFileByServerRelativeUrl('/Documents/NewPublicationTemplate.pdf')/OpenBinaryStream"
-    const response = await axios.get(getfileUrl, {
-      responseType: 'blob',
-      headers: {
-        'Content-Type': 'application/json',
-        accept: 'application/pdf'
-      }
-    })
-    let name = this.prfx + ' ' + this.pubid + ' ' + this.ltitle + '.pdf'
-    let responsedata = response.data
-    if (this.nato) {
-      url = tp1 + slash + slash + tp2 + "/_api/lists/getbytitle('NATOPublications')/RootFolder/Files/Add"
-    } else {
-      url = tp1 + slash + slash + tp2 + "/_api/lists/getbytitle('ActivePublications')/RootFolder/Files/Add"
-    }
-    // TODO: For testing we are using a different library with versioning turned on.
-    url = tp1 + slash + slash + tp2 + "/_api/lists/getbytitle('AAAPubs')/RootFolder/Files/Add"
-    let headers = {
-      Accept: 'application/json;odata=verbose',
-      'X-RequestDigest': this.digest
-    }
-    try {
-      let response = await axios({
-        url: url,
-        method: 'POST',
-        data: responsedata,
-        headers: headers
-      })
-      // get the id and process the form data into the new document
-      console.log('UPLOAD RESPONSE: ' + response)
-      let itemlink = response.data.d.ListItemAllFields.__deferred.uri
-      response = await axios({
-        method: 'GET',
-        url: itemlink,
-        headers: {
-          Accept: 'application/json;odata=verbose'
-        }
-      })
-      let metadata = response.data.d.__metadata
-      let id = response.data.d.Id
-      console.log('RESPONSE METADATA: ' + metadata)
-      // set the fields for updating
-      let itemprops = {
-        __metadata: { type: metadata.type },
-        Title: name,
-        BranchTitle: this.branch,
-        Prfx: this.prfx,
-        PubID: this.pubid,
-        LongTitle: this.ltitle,
-        DocID: this.makeGuid()
-      }
-      const uheaders = {
-        'Content-Type': 'application/json;odata=verbose',
-        Accept: 'application/json;odata=verbose',
-        'X-RequestDigest': this.digest,
-        'X-HTTP-Method': 'MERGE',
-        'If-Match': '*'
-      }
-      const config = {
-        headers: uheaders
-      }
-      response = await axios.post(metadata.uri, itemprops, config)
-      console.log('UPDATE RESPONSE: ' + response)
-      // navigate to the edit form for this item
-      this.$router.push({ name: 'Edit Publication', params: { Id: id, Nato: nato } })
-    } catch (error) {
-      console.log('Error Adding Document: ' + error)
-    }
-  } */
 }
 </script>
 
