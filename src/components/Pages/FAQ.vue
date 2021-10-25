f7fd5b4af2c510ffd97b6568b3b45d1b7586e03b
JeanG-Dev <jean.v.grunwald@gmail.com>
2021-10-25 12:33:41 -0400

    Update FAQ.vue

--- src/components/Pages/FAQ.vue
+++ src/components/Pages/FAQ.vue
@@ -1,7 +1,7 @@
 <template>
-  <b-container fluid class="contentHeight m-0 p-0" style="overflow=hidden; max-height: -webkit-fill-available;">
-    <b-overlay :show="!iframeloaded" variant="danger" class="contentHeight">
-      <b-embed type="iframe" src="https://doctrine.navy.mil/Lists/faq/AllItems.aspx?IsDlg=1" style="min-height: 200px; max-height: 850px;"></b-embed>
+  <b-container fluid class="contentHeight m-0 p-0" style="overflow=hidden;">
+    <b-overlay :show="!isIframeloaded" variant="danger" class="contentHeight">
+      <b-embed type="iframe" src="https://doctrine.navy.mil/Lists/faq/AllItems.aspx?IsDlg=1" class="contentHeight" style="overflow=auto;" @load="iframeloaded()"></b-embed>
       <template #overlay>
         <div class="text-center">
           <p id="busy-label">Loading...</p>
@@ -16,13 +16,11 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class Faq extends Vue {
-  iframeloaded?: boolean = false
+  isIframeloaded?: boolean = false
 
-  mounted() {
-    const that = this
-    window.setInterval(function() {
-      that.iframeloaded = true
-    }, 2000)
+  public iframeloaded() {
+    console.log('IFRAME LOADED')
+    this.isIframeloaded = true
   }
 }
 </script>
